a9a9727b336a6238799599353a792ad1f3030b68
sofiane khoudour <khoudoursofiane75@gmail.com>
2019-11-09 21:34:35 +0100

    fixed react navigation

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {enableScreens} from 'react-native-screens';
 import Splash from './View/Spalsh';
+import Home from './View/Home';
 class App extends Component {
   render() {
+    enableScreens();
     return <Splash></Splash>;
   }
 }
-export default App;
+const AppNavigator = createStackNavigator(
+  {
+    Splash: {
+      screen: Splash,
+      navigationOptions: {
+        title: 'MyScreen',
+        headerLeft: null,
+      },
+    },
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        title: 'MyScreen',
+        headerLeft: null,
+      },
+    },
+  },
+  {
+    initialRouteName: 'Splash',
+  },
+);
+
+export default createAppContainer(AppNavigator);
--- View/Spalsh.js
+++ View/Spalsh.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
 export default class Splash extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      this.props.navigation.navigate('Home');
+    }, 5000);
+  }
   render() {
     return (
       <View>
