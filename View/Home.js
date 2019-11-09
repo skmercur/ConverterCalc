6888fe63e5b90edf7b70a3325dd7745d4a6ba281
sofiane khoudour <khoudoursofiane75@gmail.com>
2019-11-09 22:19:36 +0100

    small changes

--- View/Home.js
+++ View/Home.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Textarea, Container } from 'native-base';
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {Textarea, Container} from 'native-base';
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      TextToConvert: '',
+      TextToConvertBin: '',
+      TextToConvertHex: '',
+      TextToConvertOct: '',
     };
   }
   StringToBin = text => {
@@ -13,27 +15,29 @@ export default class Home extends Component {
     for (var i = 0; i < text.length; i++) {
       output += text[i].charCodeAt(0).toString(2) + ' ';
     }
-    this.setState({ TextToConvert: output });
+    this.setState({TextToConvertBin: output});
   };
   StringToHexa = text => {
     let output = '';
     for (var i = 0; i < text.length; i++) {
       output += text[i].charCodeAt(0).toString(16) + ' ';
     }
-    this.setState({ TextToConvert: output });
+    this.setState({TextToConvertHex: output});
   };
   StringToOcta = text => {
     let output = '';
     for (var i = 0; i < text.length; i++) {
       output += text[i].charCodeAt(0).toString(8) + ' ';
     }
-    this.setState({ TextToConvert: output });
+    this.setState({TextToConvertOct: output});
   };
-  componentDidMount() { }
+  componentDidMount() {}
   render() {
     return (
       <Container>
-        <Text>{this.state.TextToConvert}</Text>
+        <Text>Bin : {this.state.TextToConvertBin}</Text>
+        <Text>Hex : {this.state.TextToConvertHex}</Text>
+        <Text>Oct : {this.state.TextToConvertOct}</Text>
         <Textarea
           rowSpan={20}
           onChangeText={text => {
