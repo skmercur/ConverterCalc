ed938459ba342d1c6f3af6191400a34e522ababe
sofiane khoudour <khoudoursofiane75@gmail.com>
2019-11-09 20:25:59 +0100

    new component

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
-
+import Splash from './View/Spalsh';
 class App extends Component {
   render() {
-    return (
-      <View>
-        <Text>Hi</Text>
-      </View>
-    );
+    return <Splash></Splash>;
   }
 }
 export default App;
--- View/Spalsh.js
+++ View/Spalsh.js
@@ -1,7 +1,12 @@
-import React, { Component } from 'react';
-import { } from 'react-native';
-
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
 
 export default class Splash extends Component {
-
+  render() {
+    return (
+      <View>
+        <Text>Hello</Text>
+      </View>
+    );
+  }
 }
