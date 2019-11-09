2df6c950e0339b47c07a61bc97b65dd0dc93d8d2
sofiane khoudour <khoudoursofiane75@gmail.com>
2019-11-09 21:59:03 +0100

    converter working

--- View/Home.js
+++ View/Home.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import {Textarea, Container} from 'native-base';
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      TextToConvert: '',
+    };
+  }
+  StringToBin = text => {
+    let output = '';
+    for (var i = 0; i < text.length; i++) {
+      output += text[i].charCodeAt(0).toString(2) + ' ';
+    }
+    this.setState({TextToConvert: output});
+  };
+  componentDidMount() {
+    this.StringToBin('لوحة المفاتيح العربية');
+  }
   render() {
     return (
-      <View>
-        <Text>Home is where is belong</Text>
-      </View>
+      <Container>
+        <Text>{this.state.TextToConvert}</Text>
+        <Textarea
+          rowSpan={20}
+          onChangeText={text => {
+            this.StringToBin(text);
+          }}></Textarea>
+      </Container>
     );
   }
 }
