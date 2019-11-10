a42df260c2d6b54886346e8aabf3ad92bb80d1cc
sofiane khoudour <khoudoursofiane75@gmail.com>
2019-11-10 12:12:24 +0100

    Fixed

--- View/Home.js
+++ View/Home.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Textarea, Container } from 'native-base';
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {Textarea, Container} from 'native-base';
 
 export default class Home extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -15,23 +14,17 @@ export default class Home extends Component {
   StringToHexa = text => {
     let output = '';
     let numbers = '';
-    let setHas = false;
+    let setScanOn = false;
     for (var i = 0; i < text.length; i++) {
-<<<<<<< HEAD
-      output += text[i].charCodeAt(0).toString(16) + ' ';
-=======
       if (/^\d+$/.test(text[i].charCodeAt(0).toString())) {
-setScanOn = true;
-      }else{
+        setScanOn = true;
+      } else {
         setScanOn = false;
       }
-      if(setScanOn){
-        for(var j = 0; i < )
-      }
+
       output += text[i].charCodeAt(0).toString(2) + ' ';
->>>>>>> eab8580175c77cd95621a983dcdea9594fdd7743
     }
-    this.setState({ TextToConvertHexa: output });
+    this.setState({TextToConvertHexa: output});
   };
 
   StringToBin = text => {
@@ -45,21 +38,20 @@ setScanOn = true;
         output += text[i].charCodeAt(0).toString(2) + ' ';
       }
     } else {
-      let temp = parseInt(text, nbr);
+      let temp = parseInt(text, 10);
       output = (temp >>> 0).toString(2);
-
     }
 
-    this.setState({ TextToConvertBin: output });
+    this.setState({TextToConvertBin: output});
   };
   StringToOcta = text => {
     let output = '';
     for (var i = 0; i < text.length; i++) {
       output += text[i].charCodeAt(0).toString(8) + ' ';
     }
-    this.setState({ TextToConvertOct: output });
+    this.setState({TextToConvertOct: output});
   };
-  componentDidMount() { }
+  componentDidMount() {}
   render() {
     return (
       <Container>
